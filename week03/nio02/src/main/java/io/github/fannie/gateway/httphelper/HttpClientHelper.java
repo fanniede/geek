534728c476755bb21972ee46fd04203df9ad3bc2
fannie <wenqun20@126.com>
2021-05-23 14:46:56 +0800

    week03作业

---
+++ week03/nio02/src/main/java/io/github/fannie/gateway/httphelper/HttpClientHelper.java
@@ -0,0 +1,98 @@
+package io.github.fannie.gateway.httphelper;
+import org.apache.http.HttpEntity;
+import org.apache.http.NameValuePair;
+import org.apache.http.client.ClientProtocolException;
+import org.apache.http.client.entity.UrlEncodedFormEntity;
+import org.apache.http.client.methods.CloseableHttpResponse;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClients;
+import org.apache.http.message.BasicNameValuePair;
+import org.apache.http.util.EntityUtils;
+
+import java.io.IOException;
+import java.io.UnsupportedEncodingException;
+import java.util.ArrayList;
+import java.util.List;
+
+
+public class HttpClientHelper {
+
+    public static CloseableHttpClient httpclient = HttpClients.createDefault();
+
+
+    public static void main(String[] args) throws Exception {
+        // String url = "http://hc.apache.org/httpcomponents-client-4.5.x/quickstart.html";
+        String url = "http://localhost:8888";
+
+        String text = HttpClientHelper.getAsString(url);
+        System.out.println("url: " + url + " ; response: \n" + text);
+
+        // post();
+
+    }
+
+    // GET 调用
+    public static String getAsString(String url) throws IOException {
+        HttpGet httpGet = new HttpGet(url);
+        CloseableHttpResponse response1 = null;
+        try {
+            response1 = httpclient.execute(httpGet);
+            System.out.println(response1.getStatusLine());
+            HttpEntity entity1 = response1.getEntity();
+            return EntityUtils.toString(entity1, "UTF-8");
+        } finally {
+            if (null != response1) {
+                response1.close();
+            }
+        }
+    }
+
+    // post调用
+    public static void post() {
+        // 创建默认的httpClient实例.
+        CloseableHttpClient httpclient = HttpClients.createDefault();
+        // 创建httppost
+        HttpPost httppost = new HttpPost("http://www.baidu.com/");
+        // 创建参数队列
+        List<NameValuePair> formparams = new ArrayList<NameValuePair>();
+        formparams.add(new BasicNameValuePair("type", "house"));
+        UrlEncodedFormEntity uefEntity;
+        try {
+            uefEntity = new UrlEncodedFormEntity(formparams, "UTF-8");
+            httppost.setEntity(uefEntity);
+            System.out.println("executing request " + httppost.getURI());
+            CloseableHttpResponse response = httpclient.execute(httppost);
+            try {
+                HttpEntity entity = response.getEntity();
+                if (entity != null) {
+                    System.out.println("--------------------------------------");
+                    System.out.println("Response content: " + EntityUtils.toString(entity, "UTF-8"));
+                    System.out.println("--------------------------------------");
+                }
+            } finally {
+                response.close();
+            }
+        } catch (ClientProtocolException e) {
+            e.printStackTrace();
+        } catch (UnsupportedEncodingException e1) {
+            e1.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            // 关闭连接,释放资源
+            try {
+                httpclient.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+
+
+
+
+
+}
---
+++ week03/nio02/src/main/java/io/github/fannie/gateway/outbound/okhttp/OkhttpOutboundHandler.java
@@ -0,0 +1,4 @@
+package io.github.fannie.gateway.outbound.okhttp;
+
+public class OkhttpOutboundHandler {
+}
